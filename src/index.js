798c1a510b636405ba1abd8d5ae7e28e55103d5b
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-06-25 02:27:54 -0400

    Removed unwanted Files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from './Calculator';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render( <Calculator /> , document.getElementById('root'));
-registerServiceWorker();
